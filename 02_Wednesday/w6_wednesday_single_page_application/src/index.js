c7227ca9aab305b4ae885163af9eedf9a50c50fd
Annika Ehlers <e_hlers@live.dk>
2019-09-28 19:39:43 +0200

    Added error handling for wednesday assignment

--- 02_Wednesday/w6_wednesday_single_page_application/src/index.js
+++ 02_Wednesday/w6_wednesday_single_page_application/src/index.js
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
 var table = "<tr><th>Id</th><th>Age</th><th>Name</th>" +
-                "<th>Gender</th><th>Email</th></tr>";
+    "<th>Gender</th><th>Email</th></tr>";
 
 function toHtml(array) {
     var user = array.map(item => "<tr><td>" + item.id + "</td><td>"
-            + item.age + "</td><td>"
-            + item.name + "</td><td>"
-            + item.gender + "</td><td>"
-            + item.email + "</td></tr>").join("");
+        + item.age + "</td><td>"
+        + item.name + "</td><td>"
+        + item.gender + "</td><td>"
+        + item.email + "</td></tr>").join("");
     return table + user;
 }
 
@@ -24,32 +24,39 @@ function getUsers() {
 function getUser(id) {
     var url = "http://localhost:3333/api/users/" + id;
     fetch(url)
-        .then(res => res.json())
+        .then(handleHttpErrors)
         .then(data => {
-            var user =  "<tr><td>" + data.id + "</td><td>"
-            + data.age + "</td><td>"
-            + data.name + "</td><td>"
-            + data.gender + "</td><td>"
-            + data.email + "</td></tr>";
-            document.getElementById("table").innerHTML = table+user;
+            var user = "<tr><td>" + data.id + "</td><td>"
+                + data.age + "</td><td>"
+                + data.name + "</td><td>"
+                + data.gender + "</td><td>"
+                + data.email + "</td></tr>";
+            document.getElementById("table").innerHTML = table + user;
+        })
+        .catch(err => {
+            if(err.status){
+              document.getElementById("error").innerHTML = "User with id " + id + " doesn't exist."
+            }
+            else{console.log("Network error"); }
         });
 }
 
-document.getElementById("getusersbutton").addEventListener("click", function() {
+document.getElementById("getusersbutton").addEventListener("click", function () {
     getUsers();
 });
 
-document.getElementById("buttongetuserfromid").addEventListener("click", function(){
+document.getElementById("buttongetuserfromid").addEventListener("click", function () {
     var id = document.getElementById("idinput").value;
     getUser(id);
 });
 
-// Could maybe be refactored into more methods
-document.getElementById("adduserbtn").addEventListener("click", function() {
+// Add user: Could maybe be refactored into more methods
+document.getElementById("adduserbtn").addEventListener("click", function (event) {
+    event.preventDefault();
     var _age = document.getElementById("inputage").value;
     var _name = document.getElementById("inputname").value;
     var _gender;
-    if(document.getElementById("r1").checked) {
+    if (document.getElementById("r1").checked) {
         _gender = "male";
     } else {
         _gender = "female";
@@ -69,15 +76,28 @@ document.getElementById("adduserbtn").addEventListener("click", function() {
             email: _email
         })
     }
-    fetch("http://localhost:3333/api/users/", options);
+    fetch("http://localhost:3333/api/users/", options)
+    .then(response => {
+        if(!response.ok) { throw response }
+        return response.json();
+    })
+    .then(json => {
+        document.getElementById("error").innerHTML = "Success";
+    })
+    .catch(error => {
+        error.json().then(errorMessage => {
+            document.getElementById("error").innerHTML = errorMessage.msg;
+        })
+    })
 });
 
 // A lazy solution for update
-document.getElementById("updateuserbtn").addEventListener("click", function() {
+document.getElementById("updateuserbtn").addEventListener("click", function (event) {
+    event.preventDefault();
     var _age = document.getElementById("inputage").value;
     var _name = document.getElementById("inputname").value;
     var _gender;
-    if(document.getElementById("r1").checked) {
+    if (document.getElementById("r1").checked) {
         _gender = "male";
     } else {
         _gender = "female";
@@ -98,5 +118,25 @@ document.getElementById("updateuserbtn").addEventListener("click", function() {
             email: _email
         })
     }
-    fetch("http://localhost:3333/api/users/" + id, options);
+    fetch("http://localhost:3333/api/users/" + id, options)
+    .then(response => {
+        if(!response.ok) { throw response }
+        return response.json();
+    })
+    .then(json => {
+        document.getElementById("error").innerHTML = "Success";
+    })
+    .catch(error => {
+        error.json().then(errorMessage => {
+            document.getElementById("error").innerHTML = errorMessage.msg;
+        })
+    })
 });
+
+function handleHttpErrors(res) {
+    if (!res.ok) {
+        return Promise.reject({ status: res.status, fullError: res.json() })
+    }
+    return res.json();
+}
+
